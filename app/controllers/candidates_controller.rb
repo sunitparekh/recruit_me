cbc67f33d0ee94601e216bc251304f931dd99fd3
sunitparekh <sunit@thoughtworks.com>
2010-05-29 15:24:13 +0530

    sunit: candiates filter implemented

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,83 +1,50 @@
 class CandidatesController < ApplicationController
-  # GET /candidates
-  # GET /candidates.xml
-  def index
-    @candidates = Candidate.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @candidates }
-    end
+  def prepare_filter
+    Hash[ *(params[:filter].each.collect { |key, value| [key, value] }).flatten] unless params[:filter].nil?
   end
 
-  # GET /candidates/1
-  # GET /candidates/1.xml
+  def index
+    @canidates_search = Candidate.where(prepare_filter || {})
+    @candidates = @canidates_search.all
+  end
+
   def show
     @candidate = Candidate.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @candidate }
-    end
   end
 
-  # GET /candidates/new
-  # GET /candidates/new.xml
   def new
     @candidate = Candidate.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @candidate }
-    end
   end
 
-  # GET /candidates/1/edit
   def edit
     @candidate = Candidate.find(params[:id])
   end
 
-  # POST /candidates
-  # POST /candidates.xml
   def create
     @candidate = Candidate.new(params[:candidate])
 
-    respond_to do |format|
-      if @candidate.save
-        format.html { redirect_to(@candidate, :notice => 'Candidate was successfully created.') }
-        format.xml  { render :xml => @candidate, :status => :created, :location => @candidate }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @candidate.errors, :status => :unprocessable_entity }
-      end
+    if @candidate.save
+      redirect_to(@candidate, :notice => 'Candidate was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /candidates/1
-  # PUT /candidates/1.xml
   def update
     @candidate = Candidate.find(params[:id])
 
-    respond_to do |format|
-      if @candidate.update_attributes(params[:candidate])
-        format.html { redirect_to(@candidate, :notice => 'Candidate was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @candidate.errors, :status => :unprocessable_entity }
-      end
+    if @candidate.update_attributes(params[:candidate])
+      redirect_to(@candidate, :notice => 'Candidate was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /candidates/1
-  # DELETE /candidates/1.xml
   def destroy
     @candidate = Candidate.find(params[:id])
     @candidate.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(candidates_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(candidates_url)
   end
 end
