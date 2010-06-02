e43b531a1b11b3394cd303011bde6684fc66be6e
sunitparekh <sunit@thoughtworks.com>
2010-06-02 14:01:05 +0530

    sunit:  added link to edit interview from schedule page

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ Candidate.new({:name => 'Candidate Three', :email => 'email-three@email.com',
                 :role => 'Developer', :position => 'Lead Consultant', :source => "portals:naukari", :experience => 5.5, :coordinator => 'Rakhi', :city => 'Amaravati', :office => 'Pune',
                 :contact_number => "812348776324", :status => 'In Progress', :skills => 'java, ruby', :interviews => interviews}).save!
 
-interviews = [Interview.new( :starts_at => Time.zone.now + 2.days + 1.hours, :duration => 60, :interview_type => 'Screening', :where => 'Bandipur', :interviewer_one => 'Sunit Parekh'),
+interviews = [Interview.new( :starts_at => Time.zone.now + 2.days + 1.hours, :duration => 60, :interview_type => 'Code Review', :where => 'Bandipur', :interviewer_one => 'Sunit Parekh'),
               Interview.new( :starts_at => Time.zone.now + 2.days + 2.hours + 30.minutes, :duration => 100, :interview_type => 'Office', :where => 'Corbet', :interviewer_one => 'Sunit Parekh'),
               Interview.new( :starts_at => Time.zone.now + 2.days + 2.hours + 10.minutes, :duration => 80, :interview_type => 'Casual Meeting', :where => 'Corbet', :interviewer_one => 'Sunit Parekh')]
 Candidate.new({:name => 'Candidate Four', :email => 'email-four@email.com',
